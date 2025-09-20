08e3c135270eccbe300ab2683e78454755522f64
Anudeepch <anudeepchenu1@gmail.com>
2025-09-20 20:29:19 +0530

    Update python.py

--- python.py
+++ python.py
@@ -15,7 +15,7 @@ def main():
         response = notion.databases.query(database_id=DATABASE_ID)
         pages_to_update = response.get("results")
         if not pages_to_update:
-            continue
+            return
         for page in pages_to_update:
             page_id = page["id"]
             properties = page["properties"]
